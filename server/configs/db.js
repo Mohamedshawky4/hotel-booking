c1ba9792e034d882280c41d5e3245b3f1ef76449
MohamedShawky4 <mohamedshawki4041@gmail.com>
2025-07-28 11:56:51 +0300

    13

--- server/configs/db.js
+++ server/configs/db.js
@@ -1,17 +1,20 @@
 import mongoose from "mongoose";
+
 const connectDB = async () => {
-    try {
-        console.log(process.env.MONGODB_URI);
-        mongoose.connection.on("connected", () => {
-            console.log("Mongoose is connected");
-        })
-        console.log(process.env.MONGODB_URI);
-         await mongoose.connect(`${process.env.MONGODB_URI}`);
-         
-       
-    } catch (error) {
-        console.log(error.message);
-        process.exit(1);
-    }
+  try {
+    mongoose.connection.on("connected", () => {
+      console.log("Mongoose is connected");
+    });
+
+    await mongoose.connect(process.env.MONGODB_URI, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
+
+  } catch (error) {
+    console.error("MongoDB connection error:", error.message);
+    process.exit(1);
+  }
 };
-export default connectDB
+
+export default connectDB;
